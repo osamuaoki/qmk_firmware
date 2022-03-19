2553bce3bb3385c382ef13a322399c13f7666de1
Osamu Aoki <osamu@debian.org>
2022-03-19 13:50:39 +0900

    cgg56: ortholinear 4x14 keyboard with stm32f411
     * gcg56 clone with blackpill (STM32F411)
     * keymaps: default, osamu
     * LED(Blue) indicates locked layer state (L3, L4)
    
    Signed-off-by: Osamu Aoki <osamu@debian.org>

---
+++ keyboards/cgg56/cgg56.c
@@ -0,0 +1,17 @@
+// Copyright 2022 Osamu Aoki <osamu@debian.org> (@osamuaoki)
+// SPDX-License-Identifier: GPL-2.0-or-later
+// vim:se tw=78 sts=4 ts=24 sw=4 et ai:
+#include "cgg56.h"
+
+// This is to blink blue LED on blackpill upon power up.
+void keyboard_pre_init_kb(void) {
+    setPinOutput(LED_BLUE);
+    fn_led_on(LED_BLUE);
+    wait_ms(100);
+    fn_led_of(LED_BLUE);
+    wait_ms(100);
+    fn_led_of(LED_BLUE);
+    wait_ms(100);
+    keyboard_pre_init_user();
+}
+
