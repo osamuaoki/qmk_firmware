2287062ec6865c1a7115207443a08985cf1e998c
Osamu Aoki <osamu@debian.org>
2022-09-13 19:57:43 +0900

    cgs50: use info.jason
    This avoid base QMK version issue
    
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- keyboards/cgs50/config.h
+++ keyboards/cgs50/config.h
@@ -7,11 +7,11 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
-#define DEVICE_VER   0x0001
-#define MANUFACTURER "osamuaoki"
-#define PRODUCT      "cgs50"
+//#define VENDOR_ID    0xFEED
+//#define PRODUCT_ID   0x0000
+//#define DEVICE_VER   0x0001
+//#define MANUFACTURER "osamuaoki"
+//#define PRODUCT      "cgs50"
 
 /* key matrix size */
 #define MATRIX_ROWS 8  // Rows are doubled-up
